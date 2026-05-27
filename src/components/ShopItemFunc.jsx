b1832fdfd226d0e12fe35226b6dae513ff417eba
Dilara Gilfanova <dgilfanova12@gmail.com>
2026-05-27 11:42:36 +0300

    second commit- after self-check

--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,25 +1,23 @@
 export function ShopItemFunc({ shopItem }) {
-  return (
-    <div className="container">
-      <div className="window">
-        <div className="highlight-window">
-          <div className="highlight-overlay"></div>
-        </div>
+    const { brand, title, description, descriptionFull, price, currency } = shopItem;
+
+    return (
         <div className="main-content">
-          <h2>{shopItem.brand}</h2>
-          <h1>{shopItem.title}</h1>
-          <h3>{shopItem.description}</h3>
-          <div className="description">{shopItem.descriptionFull}</div>
-          <div className="divider"></div>
-          <div className="purchase-info">
-            <div className="price">{shopItem.currency} {shopItem.price.toFixed(2)}</div>
-            <button>Добавить в корзину</button>
-          </div>
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div className="description">{descriptionFull}</div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">
+                    {currency}
+                    {price.toFixed(2)}
+                </div>
+                <button>Добавить в корзину</button>
+            </div>
         </div>
-      </div>
-      <div className="highlight-window mobile">
-        <div className="highlight-overlay"></div>
-      </div>
-    </div>
-  );
+    );
 }
